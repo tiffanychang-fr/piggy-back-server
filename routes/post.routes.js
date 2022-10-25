3a21cecbee767427cdfe16cdbf94468ae2ea5f49
Tony5300 <tony_tolen@ziggo.nl>
2022-10-25 20:56:45 +0200

    feat: create the create post route, create-order service, you can now create posts and it is stored on the backedn

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,7 +1,8 @@
 // Remark - After user logged-in, user can access to all of his/ her posts via: dashboard > my posts category
-
 const { Router } = require("express");
 
+const PostModel = require("../models/Post.model");
+const UserModel = require("../models/User.model");
 const postRouter = Router();
 
 // GET /my-posts - Render user posts history
@@ -12,10 +13,53 @@ postRouter.get("/", (req, res) => {
 
 // POST /my-posts/create - create a post that asks for help from locals
 postRouter.post("/create", (req, res) => {
-  const message = "You receive a response from the server";
+  const message =
+    "You receive a response from the server, your post is created";
   res.json({ status: "ok", message: message });
-});
+  const { title, description, city, country, budget } = req.body.requestBody;
+  const username = req.body.username;
 
+  if (
+    title === "" ||
+    description === "" ||
+    city === "" ||
+    country === "" ||
+    budget === null
+  ) {
+    res.status(400).json({
+      message: "Provide username, email, password, city, country, phone number",
+    });
+    return;
+  }
+
+  UserModel.findOne({ username })
+    .then((foundUser) => {
+      console.log(`foundUser:`, foundUser);
+      if (!foundUser) {
+        console.log("not working from this page");
+        return res.status(400).json({
+          // errorMessage: "There is not such user",
+        });
+      }
+      PostModel.create({
+        title,
+        description,
+        city,
+        country,
+        budget,
+        postBy: foundUser._id,
+      })
+        .then((createdPost) => {
+          console.log(createdPost);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 // POST /my-posts/edit/:postId - edit a single post
 postRouter.post("/edit/:postId", (req, res) => {
   const message = "You receive a response from the server";
