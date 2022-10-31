be98c194b971acb68412da8d3b9b6ae535ca710f
tiffany <changyunhsuan.fr@gmail.com>
2022-10-31 10:07:54 +0100

    get all offers from offer db.

--- routes/offer.routes.js
+++ routes/offer.routes.js
@@ -1,15 +1,39 @@
 const { Router } = require("express");
+const mongoose = require("mongoose");
 const OfferModel = require("../models/Offer.model");
+const PostModel = require("../models/Post.model");
+// const UserModel = require("../models/User.model");
 
 const offerRouter = Router();
 
-// GET /get-all-offers - Get the offers from the offer DB
+// GET /get-all-offers - Get all offers from the offer DB
 offerRouter.get("/get-all-offers", (req, res) => {
-  res.json({ status: "ok" });
+  const { userId } = req.query;
+  console.log(userId);
+
+  OfferModel.find({ receivedBy: userId })
+    .then((foundOffers) => {
+      res.json(foundOffers);
+    })
+    .catch((error) => console.log(error));
+});
+
+// GET /get-all-offers-by-post - Get all offers by post from the offer DB
+offerRouter.get("/get-all-offers-by-post", (req, res) => {
+  const { postId } = req.query;
+
+  OfferModel.find({ post: postId })
+    .then((foundOffers) => {
+      res.json(foundOffers);
+    })
+    .catch((error) => console.log(error));
 });
 
 // GET /create-offer/:postId - Make an offer to a post
 offerRouter.get("/create-offer/:postId", (req, res) => {
+  // Validation: if logged in user is post owner, then cannot make an offer to him/ herself
+  const { userId, postByUserId } = req.query;
+  if (userId === postByUserId) return;
   res.json({ status: "ok" });
 });
 
@@ -26,15 +50,22 @@ offerRouter.post("/create-offer/:postId", (req, res) => {
     return;
   }
 
-  OfferModel.create({
-    post: postId,
-    sellerMessage: sellerMessage,
-    price: price,
-    proposedBy: userId,
-    isAccepted: false,
-  })
-    .then((createdOffer) => {
-      res.json(createdOffer);
+  PostModel.findById(postId)
+    .then((foundPost) => {
+      let postById = foundPost.postBy;
+
+      OfferModel.create({
+        post: postId,
+        sellerMessage: sellerMessage,
+        price: price,
+        receivedBy: mongoose.Types.ObjectId(postById),
+        proposedBy: userId,
+        isAccepted: false,
+      })
+        .then((createdOffer) => {
+          res.json(createdOffer);
+        })
+        .catch((error) => console.log(error));
     })
     .catch((error) => console.log(error));
 });
