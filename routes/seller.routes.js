832de7cc85e7da8c0c6e16a50ddb6f39766d56aa
tiffany <changyunhsuan.fr@gmail.com>
2022-11-07 19:19:09 +0100

    add route: get all pending offers

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -3,8 +3,20 @@ const OfferModel = require("../models/Offer.model");
 
 const sellerRouter = Router();
 
-// GET /seller - Render seller page that contains stripe payment balance, pending orders, confirmed orders, live chat for confirmed orders.
-sellerRouter.get("/", (req, res) => {
+// GET /seller/pending-offers - Render seller page that contains stripe payment balance, pending orders, confirmed orders, live chat for confirmed orders.
+sellerRouter.get("/pending-offers", (req, res) => {
+  const { userId } = req.query;
+
+  OfferModel.find({ proposedBy: userId, isAccepted: false })
+    .populate("post receivedBy")
+    .then((foundPendingOffers) => {
+      res.json(foundPendingOffers);
+    })
+    .catch((error) => console.log(error));
+});
+
+// GET /seller/accepted-offers - Render seller page that contains stripe payment balance, pending orders, confirmed orders, live chat for confirmed orders.
+sellerRouter.get("/accepted-offers", (req, res) => {
   const { userId } = req.query;
 
   OfferModel.find({ proposedBy: userId, isAccepted: true })
